23816bddb05fd3eaf2663e725c6b1f018dcf9497
Damian <damian.gorenski@blumentmt.com>
2025-07-22 16:22:53 +0200

    removed all

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,4 @@
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using StockTracer.Backend;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -11,7 +9,7 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        services.AddHttpClient();
+        //services.AddHttpClient();
         services.AddSingleton<StockService>();
     })
     .Build();
